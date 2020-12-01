220d6e7af34d44fb2c42c8d2b7e0c354ccce1c28
Mark Wutka <mark.wutka@banyanhills.com>
2020-12-01 13:58:02 -0600

    Mark - don't compute intermediate cartesian product, just use loop

--- mark_wutka+commonlisp/day1.lisp
+++ mark_wutka+commonlisp/day1.lisp
@@ -4,28 +4,23 @@
        while line
        collect line)))
 
-(defun cartesian-product (l)
-  (loop
-     for x in l
-     nconc (loop for y in l
-	      collect (list x y))))
-
-(defun cartesian-product3 (l)
-  (loop
-     for x in l
-     nconc (loop for y in l
-	      nconc (loop for z in l
-		       collect (list x y z)))))
-
 (defun day1a ()
-  (let* ((numbers (mapcar #'parse-integer (read-file "day1a.txt")))
-	 (numprod (cartesian-product numbers)))
-    (apply #'* (car (remove-if-not (lambda (l) (= 2020 (apply #'+ l))) numprod)))))
+  (let ((numbers (mapcar #'parse-integer (read-file "day1a.txt"))))
+    (car (loop
+	    for x in numbers
+	    nconc (loop for y in numbers
+		     if (= 2020 (+ x y))
+		       collect (* x y))))))
 
 (defun day1b ()
-  (let* ((numbers (mapcar #'parse-integer (read-file "day1a.txt")))
-	 (numprod (cartesian-product3 numbers)))
-    (apply #'* (car (remove-if-not (lambda (l) (= 2020 (apply #'+ l))) numprod)))))
+  (let ((numbers (mapcar #'parse-integer (read-file "day1a.txt"))))
+    (car (loop
+	    for x in numbers
+	    nconc (loop for y in numbers
+		       nconc (loop for z in numbers
+				if (= 2020 (+ x y z))
+				collect (* x y z)))))))
+    
 
   
 
