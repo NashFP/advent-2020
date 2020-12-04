cdedcc2d9f966689d015c17a234ededa3edd5c35
Mark Wutka <mark.wutka@banyanhills.com>
2020-12-03 18:46:56 -0600

    Mark- formatting, comment fixes

--- mark_wutka+commonlisp/day3.lisp
+++ mark_wutka+commonlisp/day3.lisp
@@ -22,7 +22,8 @@
 ;;; The modulo computes the current position in the string. We divide i by down to account
 ;;; for skipping lines, so that we don't move right every time i is incremented, but only
 ;;; every down times       
-     if (and (eql 0 (mod i down)) (eql (aref line (mod (* (/ i down) right) (length line))) #\#))
+     if (and (eql 0 (mod i down)) 
+             (eql (aref line (mod (* (/ i down) right) (length line))) #\#))
      count 1 into trees
 
      finally (return trees)))
@@ -32,7 +33,7 @@
     (count-trees treemap 3 1)))
 
 ;;; When you provide multiple lists mapcar it calls the function with multiple values
-;;; The first list in mapcar is the number of steps down, the second list is the steps down
+;;; The first list in mapcar is the number of steps right, the second list is the steps down
 ;;;
 ;;; The #'* in the apply is just the multiply function. Common Lisp has 2 namespaces, one for
 ;;; and one for functions. The #' is how you signal to Lisp that you mean something from the
