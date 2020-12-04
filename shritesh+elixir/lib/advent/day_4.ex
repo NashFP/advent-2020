d1a9010ab0423c4a3e98cf2b41a391d57d7c93a2
Shritesh Bhattarai <shr@ite.sh>
2020-12-04 08:36:10 -0500

    Shritesh - Day 4 Part 2

--- shritesh+elixir/lib/advent/day_4.ex
+++ shritesh+elixir/lib/advent/day_4.ex
@@ -1,20 +1,51 @@
 defmodule Advent.Day4 do
-  @required_fields ~w{byr iyr eyr hgt hcl ecl pid}
-
   defp passport_fields(passport_lines) do
     passport_lines
     |> String.split(["\n", " "], trim: true)
-    |> Enum.map(&hd(String.split(&1, ":")))
+    |> Enum.into(%{}, fn str ->
+      [field, value] = String.split(str, ":")
+      {field, value}
+    end)
   end
 
-  defp valid_passport?(fields) do
-    Enum.all?(@required_fields, &(&1 in fields))
+  defp valid_passport_1?(fields) do
+    required_fields = ~w{byr iyr eyr hgt hcl ecl pid}
+    keys = Map.keys(fields)
+    Enum.all?(required_fields, &(&1 in keys))
   end
 
   def part_1(input) do
     input
     |> String.split("\n\n")
     |> Enum.map(&passport_fields/1)
-    |> Enum.count(&valid_passport?/1)
+    |> Enum.count(&valid_passport_1?/1)
+  end
+
+  defp valid_passport_2?(fields) do
+    validations = %{
+      "byr" => &(Regex.match?(~r/^\d{4}$/, &1) and String.to_integer(&1) in 1920..2002),
+      "iyr" => &(Regex.match?(~r/^\d{4}$/, &1) and String.to_integer(&1) in 2010..2020),
+      "eyr" => &(Regex.match?(~r/^\d{4}$/, &1) and String.to_integer(&1) in 2020..2030),
+      "hgt" =>
+        &case Integer.parse(&1) do
+          {h, "cm"} -> h in 150..193
+          {h, "in"} -> h in 59..76
+          _ -> false
+        end,
+      "hcl" => &Regex.match?(~r/^#[a-f,0-9]{6}$/, &1),
+      "ecl" => &(&1 in ~w{amb blu brn gry grn hzl oth}),
+      "pid" => &Regex.match?(~r/^\d{9}$/, &1)
+    }
+
+    Enum.all?(validations, fn {field, validation} ->
+      Map.has_key?(fields, field) and validation.(fields[field])
+    end)
+  end
+
+  def part_2(input) do
+    input
+    |> String.split("\n\n")
+    |> Enum.map(&passport_fields/1)
+    |> Enum.count(&valid_passport_2?/1)
   end
 end
