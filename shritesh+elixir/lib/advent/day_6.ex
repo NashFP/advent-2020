819bb50750a9c0b60708e49fd1e04ada2dfc547c
Shritesh Bhattarai <shr@ite.sh>
2020-12-06 00:50:18 -0500

    Shritesh - Day 6 - Simplify code, i guess

--- shritesh+elixir/lib/advent/day_6.ex
+++ shritesh+elixir/lib/advent/day_6.ex
@@ -1,28 +1,24 @@
 defmodule Advent.Day6 do
   def part_1(input) do
-    input
-    |> String.split("\n\n", trim: true)
-    |> Enum.map(&String.replace(&1, "\n", ""))
-    |> Enum.map(&String.codepoints/1)
-    |> Enum.map(&MapSet.new/1)
-    |> Enum.map(&Enum.count/1)
+    for group <- String.split(input, "\n\n", trim: true) do
+      group
+      |> String.replace("\n", "")
+      |> String.codepoints()
+      |> MapSet.new()
+      |> Enum.count()
+    end
     |> Enum.sum()
   end
 
   def part_2(input) do
-    input
-    |> String.split("\n\n", trim: true)
-    |> Enum.map(&String.split(&1, "\n", trim: true))
-    |> Enum.map(fn group ->
-      Enum.map(group, fn answer ->
-        answer
+    for group <- String.split(input, "\n\n", trim: true) do
+      for line <- String.split(group, "\n", trim: true) do
+        line
         |> String.codepoints()
         |> MapSet.new()
-      end)
-    end)
-    |> Enum.map(fn mapsets ->
-      Enum.reduce(mapsets, &MapSet.intersection/2)
-    end)
+      end
+      |> Enum.reduce(&MapSet.intersection/2)
+    end
     |> Enum.map(&Enum.count/1)
     |> Enum.sum()
   end
