aa320ae853df1ff379314b7d18338db4107add21
Shritesh Bhattarai <shr@ite.sh>
2020-12-06 00:28:49 -0500

    Shritesh - Day 5 - Part 2

--- shritesh+elixir/lib/advent/day_6.ex
+++ shritesh+elixir/lib/advent/day_6.ex
@@ -8,4 +8,22 @@ defmodule Advent.Day6 do
     |> Enum.map(&Enum.count/1)
     |> Enum.sum()
   end
+
+  def part_2(input) do
+    input
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(&String.split(&1, "\n", trim: true))
+    |> Enum.map(fn group ->
+      Enum.map(group, fn answer ->
+        answer
+        |> String.codepoints()
+        |> MapSet.new()
+      end)
+    end)
+    |> Enum.map(fn mapsets ->
+      Enum.reduce(mapsets, &MapSet.intersection/2)
+    end)
+    |> Enum.map(&Enum.count/1)
+    |> Enum.sum()
+  end
 end
