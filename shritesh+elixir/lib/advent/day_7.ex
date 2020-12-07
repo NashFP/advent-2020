be5bcc957e24ef28446dfdee011d43f68de435f2
Shritesh Bhattarai <shr@ite.sh>
2020-12-07 09:34:55 -0500

    Shritesh - Day 7 - Simplify regex

--- shritesh+elixir/lib/advent/day_7.ex
+++ shritesh+elixir/lib/advent/day_7.ex
@@ -1,15 +1,13 @@
 defmodule Advent.Day7 do
   @rule_regex ~r/(?<color>[\w ]*) bags contain (?<contents>.*)\./
-  @contents_regex ~r/((?<none>no other)|(?<count>\d)+ (?<color>[a-z ]+)) bags?/
+  @contents_regex ~r/(?<count>\d+) (?<color>[a-z ]+) bags?/
 
   def parse_rules(input) do
     for sentence <- String.split(input, "\n", trim: true), into: %{} do
       %{"color" => color, "contents" => contents} = Regex.named_captures(@rule_regex, sentence)
 
       bags =
-        for [none, count, color] <-
-              Regex.scan(@contents_regex, contents, capture: ["none", "count", "color"]),
-            none != "no other",
+        for [color, count] <- Regex.scan(@contents_regex, contents, capture: :all_names),
             into: %{},
             do: {color, String.to_integer(count)}
 
