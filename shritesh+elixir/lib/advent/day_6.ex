19b13505c61cf3abdfdd7a2ffe6415db1724c790
Shritesh Bhattarai <shr@ite.sh>
2020-12-07 09:51:44 -0500

    Shritesh - Cleaner for comprehensions

--- shritesh+elixir/lib/advent/day_6.ex
+++ shritesh+elixir/lib/advent/day_6.ex
@@ -11,10 +11,9 @@ defmodule Advent.Day6 do
 
   def part_2(input) do
     for group <- String.split(input, "\n\n", trim: true) do
-      for line <- String.split(group, "\n", trim: true) do
-        String.codepoints(line)
-        |> MapSet.new()
-      end
+      group
+      |> String.split("\n", trim: true)
+      |> Enum.map(&MapSet.new(String.codepoints(&1)))
       |> Enum.reduce(&MapSet.intersection/2)
       |> Enum.count()
     end
