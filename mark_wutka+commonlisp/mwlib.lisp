cf9253f9d9d55bc816882a8f2d165afd45a9f3f8
Mark Wutka <mark.wutka@banyanhills.com>
2020-12-10 11:34:19 -0600

    Mark - added takewhile function to mwlib even though unused

--- mark_wutka+commonlisp/mwlib.lisp
+++ mark_wutka+commonlisp/mwlib.lisp
@@ -9,3 +9,15 @@
 	((= (length (car lines)) 0) (split-groups (cdr lines) (cons group groups) '()))
 	(t (split-groups (cdr lines) groups (cons (car lines) group)))))
 
+(defun take (n list)
+  (if (= n 0) '()
+      (cons (car list) (take (1- n) (cdr list)))))
+
+(defun take-while (f l)
+  (if (null l) '()
+      (if (apply f (car l)) (cons (car l) (take-while f (cdr l)))
+	  (take-while f (cdr l)))))
+
+(defun drop (n list)
+  (if (= n 0) list
+      (drop (1- n) (cdr list))))
