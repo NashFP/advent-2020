0bc473ec5f0865c31b836e8bc13f699c6fe40c58
Shritesh Bhattarai <shr@ite.sh>
2020-12-11 10:30:23 -0500

    Shritesh - Day 11 - Part 1

---
+++ shritesh+elixir/test/advent/day_11_test.exs
@@ -0,0 +1,21 @@
+defmodule Advent.Day11Test do
+  use ExUnit.Case
+  alias Advent.Day11
+
+  @example """
+  L.LL.LL.LL
+  LLLLLLL.LL
+  L.L.L..L..
+  LLLL.LL.LL
+  L.LL.LL.LL
+  L.LLLLL.LL
+  ..L.L.....
+  LLLLLLLLLL
+  L.LLLLLL.L
+  L.LLLLL.LL
+  """
+
+  test "part_1" do
+    assert Day11.part_1(@example) == 37
+  end
+end
