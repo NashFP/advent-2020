0f3aa5468b5558d4c822aada373a9648fe0e1f94
Shritesh Bhattarai <shr@ite.sh>
2020-12-14 09:32:09 -0500

    Shritesh - Day 14 - Part 2

--- shritesh+elixir/lib/advent/day_14.ex
+++ shritesh+elixir/lib/advent/day_14.ex
@@ -7,9 +7,7 @@ defmodule Advent.Day14 do
 
       case location do
         "mask" ->
-          zero_mask = String.replace(value, ["X"], "1") |> String.to_integer(2)
-          one_mask = String.replace(value, ["X"], "0") |> String.to_integer(2)
-          {:mask, {zero_mask, one_mask}}
+          {:mask, value}
 
         "mem[" <> address ->
           {address, "]"} = Integer.parse(address)
@@ -19,10 +17,19 @@ defmodule Advent.Day14 do
     end
   end
 
+  defp run(input, fun) do
+    Enum.reduce(input_to_assignments(input), {%{}, {0, 0}}, fun)
+    |> elem(0)
+    |> Enum.map(&elem(&1, 1))
+    |> Enum.sum()
+  end
+
   def part_1(input) do
-    Enum.reduce(input_to_assignments(input), {%{}, {0, 0}}, fn
-      {:mask, new_mask}, {memory, _mask} ->
-        {memory, new_mask}
+    run(input, fn
+      {:mask, value}, {memory, _mask} ->
+        zero_mask = String.replace(value, "X", "1") |> String.to_integer(2)
+        one_mask = String.replace(value, "X", "0") |> String.to_integer(2)
+        {memory, {zero_mask, one_mask}}
 
       {address, value}, {memory, {zero_mask, one_mask} = mask} ->
         masked_value =
@@ -34,8 +41,57 @@ defmodule Advent.Day14 do
 
         {new_memory, mask}
     end)
-    |> elem(0)
-    |> Enum.map(&elem(&1, 1))
-    |> Enum.sum()
+  end
+
+  def masked_addresses(address, mask) do
+    base_mask =
+      Integer.to_string(address, 2)
+      |> String.pad_leading(String.length(mask), "0")
+      |> String.codepoints()
+      |> Enum.zip(String.codepoints(mask))
+      |> Enum.map(fn
+        {char, "0"} -> char
+        {_char, "1"} -> "1"
+        {_char, "X"} -> "X"
+      end)
+      |> Enum.join()
+
+    base_address = String.replace(base_mask, "X", "0") |> String.to_integer(2)
+
+    String.replace(base_mask, "1", "0")
+    |> String.codepoints()
+    |> Enum.reduce([], fn
+      "X", [] ->
+        ["0", "1"]
+
+      "X", list ->
+        Enum.concat(
+          Enum.map(list, &("0" <> &1)),
+          Enum.map(list, &("1" <> &1))
+        )
+
+      char, [] ->
+        [char]
+
+      char, list ->
+        Enum.map(list, &(char <> &1))
+    end)
+    |> Enum.map(&String.reverse/1)
+    |> Enum.map(&String.to_integer(&1, 2))
+    |> Enum.map(&bor(&1, base_address))
+  end
+
+  def part_2(input) do
+    run(input, fn
+      {:mask, new_mask}, {memory, _mask} ->
+        {memory, new_mask}
+
+      {address, value}, {memory, mask} ->
+        new_memory =
+          masked_addresses(address, mask)
+          |> Enum.reduce(memory, &Map.put(&2, &1, value))
+
+        {new_memory, mask}
+    end)
   end
 end
--- shritesh+elixir/test/advent/day_14_test.exs
+++ shritesh+elixir/test/advent/day_14_test.exs
@@ -2,14 +2,33 @@ defmodule Advent.Day14Test do
   use ExUnit.Case
   alias Advent.Day14
 
-  @example """
-  mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
-  mem[8] = 11
-  mem[7] = 101
-  mem[8] = 0
-  """
-
   test "part_1" do
-    assert Day14.part_1(@example) == 165
+    example = """
+    mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+    mem[8] = 11
+    mem[7] = 101
+    mem[8] = 0
+    """
+
+    assert Day14.part_1(example) == 165
+  end
+
+  test "masked_addresses" do
+    result = Day14.masked_addresses(42, "000000000000000000000000000000X1001X")
+
+    for expectation <- [26, 27, 58, 59] do
+      assert expectation in result
+    end
+  end
+
+  test "part_2" do
+    example = """
+    mask = 000000000000000000000000000000X1001X
+    mem[42] = 100
+    mask = 00000000000000000000000000000000X0XX
+    mem[26] = 1
+    """
+
+    assert Day14.part_2(example) == 208
   end
 end
